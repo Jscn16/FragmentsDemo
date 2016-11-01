a75cc61ab19af6b580c71227afc3dbfd2c0d1b05
Jscn16 <janis_c@gmx.de>
2016-11-01 16:30:30 +0200

    first commit

---
+++ app/src/main/java/com/example/k8035/handsonnov/AboutFragment.java
@@ -0,0 +1,24 @@
+package com.example.k8035.handsonnov;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by K8035 on 1.11.2016.
+ */
+
+public class AboutFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_about, container, false);
+        TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+        textView.setText("Main Fragment Content");
+        return rootView;
+    }
+}
